def7860b549f55d278d4363ba86d1db8fc066232
So1air <zima.blizko.2014@gmail.com>
2017-08-04 21:03:25 +0300

    +2 лабы

--- GlushchukRoman._Proga_C++._miniLabsOfCisco/Labs_Chapter6/cpa_lab_6_3_5_(1)-B.cpp
+++ GlushchukRoman._Proga_C++._miniLabsOfCisco/Labs_Chapter6/cpa_lab_6_3_5_(1)-B.cpp
@@ -2,7 +2,45 @@
 
 using namespace std;
 
+class FunnyTree {
+public:
+	virtual void cdraw() = 0;
+};
+
+class TreeWithPluses : public FunnyTree {
+public:
+	void cdraw() {
+		printf(" /\\\n/++\\");
+	}
+};
+
+class TreeWithStars : public FunnyTree {
+public:
+	void cdraw() {
+		printf(" /\\\n/**\\");
+	}
+};
+
+class TreeWithSlashs : public FunnyTree {
+public:
+	void cdraw() {
+		printf(" /\\\n//\\\\");
+	}
+};
+
+
 int Go1(void) {
+	FunnyTree* trees[3];
+	0[trees] = new TreeWithSlashs;
+	1[trees] = new TreeWithStars;
+	2[trees] = new TreeWithPluses;
+
+	for(int i = 0; i < 3; i++) {
+		printf("Drawing %d\n", i + 1);
+		trees[i]->cdraw();
+		delete trees[i];
+		cout << endl;
+	}
 	
 	return 0;
 }
--- GlushchukRoman._Proga_C++._miniLabsOfCisco/Labs_Chapter6/cpa_lab_6_3_6_(1)-B.cpp
+++ GlushchukRoman._Proga_C++._miniLabsOfCisco/Labs_Chapter6/cpa_lab_6_3_6_(1)-B.cpp
@@ -1,8 +1,72 @@
 #include <iostream>;
+#include <string>;
 
 using namespace std;
 
+enum HorizontalLabel{A, B, C, D, E, F, G, H};
+
+class Piece {
+protected:
+	HorizontalLabel horizontal;
+	unsigned short vertical;
+public:
+	virtual bool stepvalidation(HorizontalLabel, unsigned short) = 0; 
+	Piece(HorizontalLabel horiz, unsigned short vert): horizontal(horiz), vertical(vert) {
+		if (vertical > 7)
+			vertical = 7;
+		if (vertical % 2)
+			switch (horizontal)
+			{case HorizontalLabel::B:
+			case HorizontalLabel::D:
+			case HorizontalLabel::F:
+			case HorizontalLabel::H:
+				vertical--;			
+			default:
+				break;
+			}
+		else 
+			switch (horizontal)
+			{
+			case HorizontalLabel::A:
+			case HorizontalLabel::C:
+			case HorizontalLabel::E:
+			case HorizontalLabel::G:
+				vertical++;
+			default:
+				break;
+			}
+	}
+};
+
+class Man : public Piece {
+public:
+	bool stepvalidation(HorizontalLabel horiz_coord, unsigned short vert_coord) {
+		return (vert_coord < 8) && ((vert_coord - vertical) == abs(horiz_coord - horizontal) == 1);
+	}
+
+	Man(HorizontalLabel h, unsigned short v) : Piece(h, v) { }
+};
+
+class King : public Piece {
+public:
+	bool stepvalidation(HorizontalLabel horiz_coord, unsigned short vert_coord) {
+		return (vert_coord < 8) && (abs(vert_coord - vertical) == abs(horiz_coord - horizontal));
+	}
+
+	King(HorizontalLabel h, unsigned short v) : Piece(h, v) { }
+};
+
 int Go2(void) {
-	
+	Piece* pieces[2];
+	pieces[0] = new Man(HorizontalLabel::B, 0);
+	pieces[1] = new King(HorizontalLabel::B, 0);
+
+	cout << (pieces[0]->stepvalidation(HorizontalLabel::C, 1) ? "true" : "false")  << endl;
+	cout << (pieces[1]->stepvalidation(HorizontalLabel::D, 2) ? "true" : "false") << endl; 
+	cout << (pieces[0]->stepvalidation(HorizontalLabel::D, 2) ? "true" : "false") << endl;
+
+	delete pieces[0];
+	delete pieces[1];
+
 	return 0;
 }
